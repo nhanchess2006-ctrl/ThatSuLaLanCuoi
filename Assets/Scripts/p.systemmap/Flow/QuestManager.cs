b4e03a7033ace360481c0875b3dbfc3011517cdf
Trần Hoài Phương <madara932000@gmail.com>
2026-08-05 22:56:24 +0700

    Reapply "Merge pull request #9 from nhanchess2006-ctrl/Phuong"
    This reverts commit b99d122daed13b476e4c4cbec2005a6d84c4a6fc.

---
+++ Assets/Scripts/p.systemmap/Flow/QuestManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public static QuestManager Instance;
+
+    [SerializeField] private QuestUI questUI;
+
+    public int currentFlower = 0;
+    public int targetFlower = 20;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        questUI.UpdateProgress(currentFlower, targetFlower);
+    }
+
+    public void CollectFlower()
+    {
+        currentFlower++;
+
+        questUI.UpdateProgress(currentFlower, targetFlower);
+
+        if(currentFlower >= targetFlower)
+        {
+            Debug.Log("Hoàn thành nhiệm vụ!");
+        }
+    }
+}
